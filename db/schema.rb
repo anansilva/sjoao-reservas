71afec8b2a93d6a2132b36cc4ae1fa8f766d902d
Ana Nunes da Silva <ananunesdasilva@MacBook-Air-4.local>
2021-05-28 19:02:18 +0100

    Generate Booking model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_203339) do
+ActiveRecord::Schema.define(version: 2021_05_28_175903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "check_in", null: false
+    t.date "check_out", null: false
+    t.bigint "house_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["house_id"], name: "index_bookings_on_house_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "houses", force: :cascade do |t|
     t.string "name"
@@ -35,4 +46,6 @@ ActiveRecord::Schema.define(version: 2021_05_24_203339) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "houses"
+  add_foreign_key "bookings", "users"
 end
